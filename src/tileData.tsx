5cccb080a750be28c5001240ccccf4551263b75b
Timothy Coleman <timhc22@gmail.com>
2021-06-05 15:40:36 +0100

    Add more images so can show nav hiding

--- src/tileData.tsx
+++ src/tileData.tsx
@@ -25,6 +25,30 @@ const tileData = [
     author: 'author',
     cols: 2,
   },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
 ];
 
 export default tileData;
