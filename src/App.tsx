a0f5944c5b512467d60e5de1121be073e8b6393f
Timothy Coleman <timhc22@gmail.com>
2021-06-05 15:30:03 +0100

    Start adding some gallery libraries

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,26 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Slide from '@material-ui/core/Slide';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import tileData from './tileData';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      // width: 500,
+      // height: 450,
+    },
+  }),
+);
 
 interface Props {
   children: React.ReactElement;
@@ -29,6 +49,9 @@ function HideOnScroll(props: Props) {
 
 // function App(props: Props) {
 function App() {
+
+  const classes = useStyles();
+
   return (
     <div className="App">
       {/*<header className="App-header">*/}
@@ -49,6 +72,14 @@ function App() {
         </HideOnScroll>
         <Toolbar />
         <Container>
+
+          <GridList cellHeight={160} className={classes.gridList} cols={3}>
+            {tileData.map((tile) => (
+              <GridListTile key={tile.img} cols={tile.cols || 1}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
 
         </Container>
       </React.Fragment>
---
+++ src/tileData.tsx
@@ -0,0 +1,30 @@
+import image from 'path/to/image.jpg';
+
+const tileData = [
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/200/300',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
+  },
+];
+
+export default tileData;
